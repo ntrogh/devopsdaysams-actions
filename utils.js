189f4faa448b3e3d8bedc7cd60bec8d0c6d7b01a
Nick Trogh <ntrogh@hotmail.com>
2020-10-05 08:39:43 +0000

    Use got instead of fetch

--- utils.js
+++ utils.js
@@ -1,19 +1,14 @@
 const fetch = require("node-fetch");
+const got = require("got");
 
 const utils = {
     // Invoke the API to post a quote to the Zen wall
     // baseUrl: base URL of the API
     // repoUrl: the URL to the GitHub repository, passed as a query parameter to the API endpoint
     addToWall: async function (baseUrl, repoUrl) {
-        const response = await fetch(`${baseUrl}?repoUrl=${repoUrl}`, { method: 'POST' });
-        const message = await response.text();
-        if (response.status != 200) {
-            console.log(message);
-            throw new Error(
-                `Received ${response.status} status code. Reason: ${message}`
-            );
-        }
-        return message;
+        const response = await got.post(`${baseUrl}?repoUrl=${repoUrl}`, { json: true });
+        console.log(response.body);
+        return response.body;
     }
 
 };
