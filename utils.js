436487c0d23898b19edf83a7c35a40ad2cc1b80d
Nick Trogh <ntrogh@hotmail.com>
2020-10-05 09:00:39 +0000

    parse api result to show quote

--- utils.js
+++ utils.js
@@ -1,14 +1,18 @@
 const fetch = require("node-fetch");
-const got = require("got");
 
 const utils = {
     // Invoke the API to post a quote to the Zen wall
     // baseUrl: base URL of the API
     // repoUrl: the URL to the GitHub repository, passed as a query parameter to the API endpoint
     addToWall: async function (baseUrl, repoUrl) {
-        const response = await got.post(`${baseUrl}?repoUrl=${repoUrl}`, { json: true });
-        console.log(response.body);
-        return response.body;
+        const response = await fetch(`${baseUrl}?repoUrl=${repoUrl}`, { method: 'POST' });
+        const message = await response.text();
+        if (response.status != 200) {
+            throw new Error(
+                `Received ${response.status} status code. Reason: ${message}`
+            );
+        }
+        return message;
     }
 
 };
